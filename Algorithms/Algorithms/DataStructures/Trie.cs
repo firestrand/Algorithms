9d20ae4637eb7fdceef56aa97938ea3eae3ac7e4
Travis Silvers <firestrand@gmail.com>
2021-06-23 15:46:08 -0700

    Implementation of Trie

---
+++ Algorithms/Algorithms/DataStructures/Trie.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.DataStructures
+{
+    public class Trie
+    {
+        private sealed class Node
+        {
+            private readonly char _character;
+            public int Count { get; set; }
+            public bool IsWordEnding { get; set; }
+            private readonly Dictionary<char, Node> _children = new Dictionary<char, Node>();
+
+            public Node(char character)
+            {
+                this._character = character;
+            }
+
+            public void AddChild(char character, Node node)
+            {
+                this._children.Add(character, node);
+            }
+
+            public Node GetChild(char character)
+            {
+                return this._children.GetValueOrDefault(character);
+            }
+
+            public void DeleteChild(char character)
+            {
+                this._children.Remove(character);
+            }
+            private bool Equals(Node other)
+            {
+                return _character == other._character && Equals(_children, other._children) && Count == other.Count && IsWordEnding == other.IsWordEnding;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return ReferenceEquals(this, obj) || obj is Node other && Equals(other);
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(_character, _children);
+            }
+        }
+
+        private const char RootChar = '\0';
+        private readonly Node _root = new Node(RootChar);
+
+        public void ValidateKey(string key)
+        {
+            if (String.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("key can not be null or empty", key);
+            }
+        }
+
+        public void ValidateNum(int num)
+        {
+            if (num <= 0)
+            {
+                throw new ArgumentException("num must be > 0", num.ToString());
+            }
+        }
+
+        public bool Insert(string key, int num = 1)
+        {
+            ValidateKey(key);
+            ValidateNum(num);
+
+            var node = _root;
+            bool isNodeCreated = false;
+            bool isPrefix = false;
+
+            foreach (var c in key)
+            {
+                var nextNode = node.GetChild(c);
+                if (nextNode == null)
+                {
+                    nextNode = new Node(c);
+                    node.AddChild(c, nextNode);
+                    isNodeCreated = true;
+                }
+                else if(nextNode.IsWordEnding)
+                {
+                    isPrefix = true;
+                }
+
+                nextNode.Count += num;
+                node = nextNode;
+            }
+
+            if (!_root.Equals(node))
+            {
+                node.IsWordEnding = true;
+            }
+            return isPrefix || !isNodeCreated;
+        }
+
+        public bool Delete(string key, int num = 1)
+        {
+            if (!Contains(key))
+            {
+                throw new ArgumentException("key not present in Trie", key);
+            }
+
+            ValidateNum(num);
+
+            var node = _root;
+            foreach (var c in key)
+            {
+                var nextNode = node.GetChild(c);
+                nextNode.Count -= num;
+
+                if (nextNode.Count <= 0)
+                {
+                    node.DeleteChild(c);
+                    return true;
+                }
+
+                node = nextNode;
+            }
+
+            return true;
+        }
+
+        public int Count(string key)
+        {
+            ValidateKey(key);
+
+            var node = _root;
+            foreach (var c in key)
+            {
+                if (node == null)
+                {
+                    return 0;
+                }
+
+                node = node.GetChild(c);
+            }
+
+            if (node != null)
+            {
+                return node.Count;
+            }
+            return 0;
+        }
+
+        public bool Contains(string key)
+        {
+            return Count(key) > 0;
+        }
+
+    }
+}
---
+++ Algorithms/AlgorithmsTests/DataStructures/TrieTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DataStructures.Tests
+{
+    [TestClass]
+    public class TrieTests
+    {
+        private Trie _trie;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _trie = new Trie();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Insert_WhenKeyNull_ThenException()
+        {
+            _trie.Insert(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Insert_WhenNumZero_ThenException()
+        {
+            _trie.Insert("key", 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Delete_WhenKeyNull_ThenException()
+        {
+            _trie.Delete(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Delete_WhenKeyNotPresent_ThenException()
+        {
+            _trie.Delete("foo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Delete_WhenNumZero_ThenException()
+        {
+            _trie.Insert("key");
+            _trie.Delete("key", -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Contains_WhenKeyNull_ThenException()
+        {
+            _trie.Contains(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Count_WhenKeyNull_ThenException()
+        {
+            _trie.Count(null);
+        }
+
+        [TestMethod]
+        public void Insert_WithKey_ThenContainsKey()
+        {
+            var key = "foo";
+            _trie.Insert(key);
+            Assert.IsTrue(_trie.Contains(key));
+        }
+
+        [TestMethod]
+        public void InsertAndDelete_WhenSameKey_ThenContainsFalse()
+        {
+            var key = "foo";
+            _trie.Insert(key);
+            _trie.Delete(key);
+            Assert.IsFalse(_trie.Contains(key));
+        }
+
+        [TestMethod]
+        public void InsertAndDelete_WhenPrefixRemovedValueRemains_ThenContainsTrue()
+        {
+            var key = "foo";
+            _trie.Insert(key);
+            _trie.Insert("foot");
+            _trie.Delete(key);
+            Assert.IsTrue(_trie.Contains(key));
+        }
+
+        [TestMethod]
+        public void Insert_WhenSameKeyThreeTimes_ThenCountThree()
+        {
+            var key = "foo";
+            _trie.Insert(key);
+            _trie.Insert(key);
+            _trie.Insert(key);
+            Assert.AreEqual(3, _trie.Count(key));
+        }
+
+        [TestMethod]
+        public void Insert_WhenSamePrefixThreeTimes_ThenCountThree()
+        {
+            _trie.Insert("fooz");
+            _trie.Insert("foobat");
+            _trie.Insert("foomon");
+            Assert.AreEqual(3, _trie.Count("foo"));
+        }
+
+        [TestMethod]
+        public void Count_WhenLastCharMismatch_ThenCountZero()
+        {
+            _trie.Insert("foo");
+            Assert.AreEqual(0, _trie.Count("fot"));
+        }
+
+        
+    }
+}
